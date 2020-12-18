c145cf8d11b9abb505aa8e848e4a1e3de39e4f95
Mark Cameron (3T) <markcame@microsoft.com>
2020-12-18 14:17:34 -0600

    refactored

--- src/space_1.py
+++ src/space_1.py
@@ -1,53 +1,30 @@
-class DataConvert:
+import functools
 
+_VALID_COMMANDS = frozenset({"b", "p", "k", "m", "g", "t", "pb", "help", "quit"})
+
+
+def b_to_pages(num, *, reverse=False, power=-1):
+    return (num * 1024 ** power) / 8 if not reverse else (num * 8) / 1024 ** power
+
+
+conversions = {"b": b_to_pages, "p": lambda num, **kwargs: num}
+for power, scale in enumerate(["k", "m", "g", "t", "pb"]):
+    conversions[scale] = functools.partial(b_to_pages, power=power)
+
+del power, scale
+
+
+class DataConvert:
     def __init__(self, data, valid_args):
-        self.prep = {'p': []}
+        self.prep = {"p": []}
         self.pages = 0
         self.data = data
         self.valid = valid_args
 
-    # set up conversions to pages and back
-
-    def b_to_pages(self, num, reverse=False):
-        if reverse == False:
-            return (num / 1024) / 8
-        elif reverse == True:
-            return (num * 8) * 1024
-
-    def k_to_pages(self, num, reverse=False):
-        if reverse == False:
-            return num / 8
-        elif reverse == True:
-            return num * 8
-
-    def m_to_pages(self, num, reverse=False):
-        if reverse == False:
-            return self.k_to_pages(num * 1024)
-        elif reverse == True:
-            return (num * 8) / 1024
-
-    def g_to_pages(self, num, reverse=False):
-        if reverse == False:
-            return self.k_to_pages(num * 1024**2)
-        elif reverse == True:
-            return (num * 8) / 1024**2
-
-    def t_to_pages(self, num, reverse=False):
-        if reverse == False:
-            return self. k_to_pages(num * 1024**3)
-        elif reverse == True:
-            return (num * 8) / 1024**3
-
-    def pb_to_pages(self, num, reverse=False):
-        if reverse == False:
-            return self.k_to_pages(num * 1024**4)
-        elif reverse == True:
-            return (num * 8) / 1024**4
-
     # data cleaning
     def is_help(self):
-        if 'help' in self.prep.keys():
-            return '''Format your input:  
+        if "help" in self.prep.keys():
+            return """Format your input:  
 Input_type num num num Input_type num num num...
 
 Input_Types
@@ -60,34 +37,36 @@ t = terabyte
 
 quit = quit
 help = help
-'''
+"""
 
     def is_quit(self):
         return False
 
     def detect_zero_sum(self):
         for k, v in self.prep.items():
-            if k == 'p':
+            if k == "p":
                 continue
             elif sum(v) == 0:
-                return 'Zero sum detected. Cannot divide by zero.'
+                return "Zero sum detected. Cannot divide by zero."
 
     def validate_input(self):
         for x in self.data:
             if type(x) == str:
                 if x.lower() in self.valid:
                     hold = x.lower()
-                    if hold == 'quit':
+                    if hold == "quit":
                         return self.is_quit()
-                    elif hold == 'help':
+                    elif hold == "help":
                         print(self.is_help())
-                        return 1, 'Restarting'
+                        return 1, "Restarting"
                     elif hold not in self.prep.keys():
                         self.prep[hold] = []
             elif type(x) == float:
-                self.prep[hold].append(x)
+                self.prep[hold].append(
+                    x
+                )  # hold comes from a previous iteration of the loop
             else:
-                return 0, 'Invalid input.'
+                return 0, "Invalid input."
         detect_zeros = self.detect_zero_sum()
         if detect_zeros is not None:
             return detect_zeros
@@ -99,33 +78,18 @@ help = help
     # calculate all self.prep.values() to pages
 
     def convert_to_pages(self):
-        for k in self.prep.keys():
-            if k == 'b':
-                self.prep[k] = self.b_to_pages(self.prep[k])
-            elif k == 'k':
-                self.prep[k] = self.k_to_pages(self.prep[k])
-            elif k == 'm':
-                self.prep[k] = self.m_to_pages(self.prep[k])
-            elif k == 'g':
-                self.prep[k] = self.g_to_pages(self.prep[k])
-            elif k == 't':
-                self.prep[k] = self.t_to_pages(self.prep[k])
-            elif k == 'pb':
-                self.prep[k] = self.pb_to_pages(self.prep[k])
+        for k in self.prep:
+            self.prep[k] = conversions[k](self.prep[k])
 
     def add_to_pages(self):
         total_pages = 0
         for v in self.prep.values():
             total_pages += v
-        self.prep['p'] = total_pages
+        self.prep["p"] = total_pages
 
     def recalculate_values(self):
-        self.prep['b'] = self.b_to_pages(self.prep['p'], True)
-        self.prep['k'] = self.k_to_pages(self.prep['p'], True)
-        self.prep['m'] = self.m_to_pages(self.prep['p'], True)
-        self.prep['g'] = self.g_to_pages(self.prep['p'], True)
-        self.prep['t'] = self.t_to_pages(self.prep['p'], True)
-        self.prep['pb'] = self.pb_to_pages(self.prep['p'], True)
+        for k, convert in conversions.items():
+            self.prep[k] = convert(self.prep["p"], reverse=True)
 
     def results(self):
         self.validate_input()
@@ -136,51 +100,37 @@ help = help
         return self.prep
 
 
-class Application:
-    def __init__(self):
-        self.valid = ['b', 'p', 'k', 'm', 'g',
-                      't', 'pb', 'help', 'quit', int, float]
+def request_data():
+    return input('Please enter values or type "help" for help\n')
 
-    def request_data(self):
-        return input('Please enter values or type "help" for help\n')
 
-    def format_data(self, data, valid_args):
-        listify = data.split()
-        for x in listify:
-            if x.lower() in self.valid:
-                continue
-            elif x not in self.valid:
-                try:
-                    listify[listify.index(x)] = float(x)
-                except:
-                    print('Invalid input')
-                    break
-        return listify
+def format_data(data):
+    try:
+        return [x if x.lower() in _VALID_COMMANDS else float(x) for x in data.split()]
+    except ValueError:
+        print("Invalid input")
 
-    def return_str(self, data):
-        print('''
+
+def data_to_str(data):
+    return f"""
 Results
 ============
-Bytes : {0[b]}
-Pages : {0[p]}
-Kilobytes : {0[k]}
-Megabytes : {0[m]}
-Gigabytes : {0[g]}
-Terabytes : {0[t]}
-Petabytes : {0[pb]}
+Bytes : {data['b']}
+Pages : {data['p']}
+Kilobytes : {data['k']}
+Megabytes : {data['m']}
+Gigabytes : {data['g']}
+Terabytes : {data['t']}
+Petabytes : {data['pb']}
 
-'''.format(data))
-
-    def run_it(self):
-        a = DataConvert(self.format_data(
-            self.request_data(), self.valid), self.valid)
-        return self.return_str(a.results())
+"""
 
 
-if __name__ == '__main__':
-    running = True
-    while running == True:
-        a = Application()
-        b = a.run_it()
-        if b == False:
-            running = False
+def main():
+    formatted_data = format_data(request_data())
+    a = DataConvert(formatted_data, _VALID_COMMANDS)
+    print(data_to_str(a.results()))
+
+
+if __name__ == "__main__":
+    main()
